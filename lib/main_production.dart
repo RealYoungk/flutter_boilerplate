9c7dc8bca5c6fa0d1997ff846912d872161ff64e
realyoungk_mac_pro <real_youngk@naver.com>
2022-07-25 18:30:31 +0900

    first init

---
+++ lib/main_production.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/widgets.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // final todosApi = LocalStorageTodosApi(
+  //   plugin: await SharedPreferences.getInstance(),
+  // );
+  //
+  // bootstrap(todosApi: todosApi);
+}
